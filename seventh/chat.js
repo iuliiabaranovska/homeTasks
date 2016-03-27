cb44db010f4a68bcb6ee978671a9a1c28e6ed04d
iuliiabaranovska <iuliia.baranovska@gmail.com>
2016-03-27 14:12:42 +0300

    Message on console via observer pattern

---
+++ seventh/chat.js
@@ -0,0 +1,69 @@
+var chat = {
+
+    //publisher properties
+    subscribers: {
+        any: []
+    },
+
+    subscribe: function(callback, type) {
+        type = type || 'any';
+        if (typeof this.subscribers[type] === 'undefined') {
+            this.subscribers[type] = [];
+        }
+        this.subscribers[type].push(callback);
+    },
+
+    unsubscribe: function(callback, type) {
+        type = type || 'any';
+        var subscribers = this.subscribers[type];
+        for (var i = 0; i < subscribers.length; i += 1) {
+            if (subscribers[i] === callback) {
+                subscribers.splice(i, 1);
+            }
+        }
+    },
+
+    publish: function(data, type) {
+        type = type || 'any';
+        var subscribers = this.subscribers[type];
+        if (subscribers !== undefined) {
+            for (var i = 0; i < subscribers.length; i += 1) {
+                subscribers[i](data);
+            }
+        }
+    },
+
+    //chat wrapper
+    sendMessage: function(message) {
+        this.publish(message, message.recipient);
+    }
+};
+
+var user = {
+    receivedMessage: function(message) {
+        console.log(message);
+    }
+}
+
+var Message = (function() {
+
+    function Message(recipient, messageText) {
+    	
+        if (!(this instanceof Message)) {
+            return new Message(recipient, messageText);
+        }
+
+        this.recipient = recipient;
+        this.messageText = messageText;
+        this.timeSent = new Date();
+    }
+
+    return Message;
+}());
+
+
+chat.subscribe(user.receivedMessage, "stepan");
+
+chat.sendMessage(new Message("petro", "hello, petro!!!"));
+chat.sendMessage(new Message("stepan", "hello, petro!!!"));
+chat.sendMessage(new Message("petro", "hello, petro, one more time!!!"));
