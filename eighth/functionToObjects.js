775d132ea896b295bc4296228e45a10e05cd6af4
iuliiabaranovska <iuliia.baranovska@gmail.com>
2016-04-01 15:05:20 +0300

    userList has been modified

--- eighth/functionToObjects.js
+++ eighth/functionToObjects.js
@@ -66,51 +66,15 @@ var UserManager = function(usersList) {
     }
 }
 
-var usersList = [{
-    name: 'John',
-    info: 'likes bowling',
-    second: 'Second',
-    avatar: 'http://www.iconarchive.com/download/i51046/hopstarter/halloween-avatars/Jason.ico',
-    newfield: 'asdasdasdasd',
-    skills: ['EcmaScript', 'PHP', 'Java'],
-    fullTime: true,
-    phone: "617-123-4567",
-    address: {
-        city: "Boston",
-        state: "MA",
-        zip: "02106"
-    }
-}, {
-    name: 'Pavel',
-    info: 'plays footbal',
-    second: 'Second',
-    avatar: 'http://www.iconarchive.com/download/i51026/hopstarter/halloween-avatars/Alien.ico',
-    newfield: 'asdasdasdasd',
-    skills: ['JavaScript', 'C#', 'Paskal'],
-    fullTime: false,
-    phone: "617-987-6543",
-    address: {
-        city: "London",
-        state: "GH",
-        zip: "45676"
-    }
-}, {
-    name: 'Igor',
-    info: 'drinks a lot',
-    second: 'Second',
-    avatar: 'http://files.softicons.com/download/internet-cons/halloween-avatars-icons-by-deleket/ico/Zombie%202.ico',
-    newfield: 'asdasdasdasd',
-    skills: ['C++', 'Ruby'],
-    fullTime: true,
-    phone: "617-111-2323",
-    address: {
-        city: "Amsterdam",
-        state: "JJ",
-        zip: "69696"
-    }
-}];
+var usersList = [
+    new User('John', 'likes bowling', 'Second', 'http://www.iconarchive.com/download/i51046/hopstarter/halloween-avatars/Jason.ico',
+        'asdasdasdasd', ['EcmaScript', 'PHP', 'Java'], true, "617-123-4567", "Boston", "MA", "02106"),
+    new User('Pavel', 'plays footbal', 'Second', 'http://www.iconarchive.com/download/i51026/hopstarter/halloween-avatars/Alien.ico',
+        'asdasdasdasd', ['JavaScript', 'C#', 'Paskal'], false, "617-987-6543", "London", "GH", "45676"),
+    new User('Igor', 'drinks a lot', 'Second', 'http://files.softicons.com/download/internet-cons/halloween-avatars-icons-by-deleket/ico/Zombie%202.ico',
+        'asdasdasdasd', ['C++', 'Ruby'], true, "617-111-2323", "Amsterdam", "JJ", "69696")
+];
 
 var userManager = new UserManager(usersList);
 userManager.setUserInfoNodeHTML('');
 userManager.drawUsers(usersList);
-
